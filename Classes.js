49825afd051f68c80b45235bd659b362cef44d27
Abhishek Duggal <abhiduggal8@gmail.com>
2018-10-23 21:34:14 -0700

    Practice Kata

--- Classes.js
+++ Classes.js
@@ -12,3 +12,27 @@ class Manager extends Employee {
 }
 
 var mang1 = new Manager("Abhi", "Duggal", "manager@gmail.com", 36, 0);
+
+class ProgressiveManager extends Manager {
+  constructor(first_name, last_name, email, age, reports, title, bonus) {
+    super(first_name, last_name, email, age, reports);
+    this.title = "Not a manager";
+    this.bonus = 0;
+  }
+
+  setTitle(emp) {
+    if (emp === 0) {
+      return "Not a manager";
+    } else if (emp >= 1 && emp <= 3) {
+      return "Barely Manager";
+    } else if (emp >= 4 && emp <= 10) {
+      return "Mostly Manager";
+    } else if (emp >= 11 && emp <= 50) {
+      return "Manager";
+    } else if (emp >= 51 && emp <= 100) {
+      return "Manager Plus";
+    } else if (emp >= 101) {
+      return "Bestest Manager";
+    }
+  }
+}
