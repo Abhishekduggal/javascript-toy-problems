62b3038077863a5cf1f48ed42e6a604c2e9268b5
Abhishek Duggal <abhiduggal8@gmail.com>
2018-11-01 10:21:27 -0700

    Practice Kata

---
+++ ES6.js
@@ -0,0 +1,50 @@
+var names = ["Alexandria", "Matthew", "Joe"];
+
+names.every(function(name) {
+  return name.length > 4;
+});
+
+names.some(function(name) {
+  return name.length > 4;
+});
+
+var requests = [
+  { url: "/photos", status: "complete" },
+  { url: "/albums", status: "pending" },
+  { url: "/users", status: "failed" }
+];
+
+var inProgress;
+
+inProgress = requests.some(function(request) {
+  return request.status === "pending";
+});
+
+// Balance parnethesis using Reduce Helper function
+
+function checkPattern(str) {
+  return !str.split("").reduce(function(acc, char) {
+    if (acc < 0) {
+      return acc;
+    }
+    if (acc === "(") {
+      return ++acc;
+    }
+    if (acc === ")") {
+      return --acc;
+    }
+
+    return acc;
+  }, 0);
+}
+checkPattern("(()((())))");
+
+// Distance Travelled
+
+var trips = [{ distance: 34 }, { distance: 12 }, { distance: 1 }];
+
+var totalDistance;
+
+totalDistance = trips.reduce(function(sum, trip) {
+  return sum + trip.distance;
+}, 0);
