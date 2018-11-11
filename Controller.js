fae1de68c64613e5e11938e34cf6ed4e73bcb4fd
Abhishek Duggal <abhiduggal8@gmail.com>
2018-11-10 18:29:09 -0800

    Practice Kata

--- Controller.js
+++ Controller.js
@@ -72,3 +72,35 @@ const youtubesearch = (req, res, next) => {
 module.exports = {
   youtubesearch
 };
+
+const createSpec = (req, res, next) => {
+  let {
+    productid,
+    temp,
+    productspeed,
+    productdensity,
+    rejects,
+    waterpressure,
+    drylevel,
+    machinespeed
+  } = req.body;
+
+  const db = req.app.get("db");
+  db.specifications
+    .create_spec([
+      productid,
+      parseInt(temp, 10),
+      parseInt(productspeed, 10),
+      parseInt(productdensity, 10),
+      parseInt(rejects, 10),
+      parseInt(waterpressure, 10),
+      parseFloat(drylevel).toFixed(2),
+      parseInt(machinespeed, 10)
+    ])
+    .then(response => {
+      res.status(200).send(response);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+};
