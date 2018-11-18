bad3bf23238d992843738ce78ef01e6ab61d2575
Abhishek Duggal <abhiduggal8@gmail.com>
2018-11-17 19:21:03 -0800

    Practice Kata

--- Controller.js
+++ Controller.js
@@ -212,3 +212,30 @@ const youtubesearch = (req, res, next) => {
 module.exports = {
   youtubesearch
 };
+
+// Consistent read from the database
+
+const readRejects = (req, res, next) => {
+  const db = req.app.get("db");
+  db.charts
+    .get_rejects()
+    .then(response => {
+      res.status(200).send(response);
+    })
+    .catch(err => res.status(500).send(err));
+};
+
+const readHrUsersFacility = (req, res, next) => {
+  const db = req.app.get("db");
+  db.charts
+    .get_hrUsers_facility()
+    .then(response => {
+      res.status(200).send(response);
+    })
+    .catch(err => res.status(500).send(err));
+};
+
+module.exports = {
+  readRejects,
+  readHrUsersFacility
+};
