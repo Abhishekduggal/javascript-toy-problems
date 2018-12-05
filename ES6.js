731f46b9fc182b86a59f646ed7d4f485c223ca3c
Abhishek Duggal <abhiduggal8@gmail.com>
2018-12-04 19:40:38 -0800

    Practice Kata

--- ES6.js
+++ ES6.js
@@ -514,4 +514,16 @@ function squareEveryNum(int) {
 // squareEveryNum(9119) // ==> 811181
 squareEveryNum(4232); //==> 16494
 
-// Note: The function should accept an integer and return an integer
+// Note: The function should accept an integer and return an int
+
+// Practice
+// Pete likes to bake some cakes. He has some recipes and ingredients. Unfortunately he is not good in maths. Can you help him to find out, how many cakes he could bake considering his recipes?
+
+// Write a function cakes(), which takes the recipe (object) and the available ingredients (also an object) and returns the maximum number of cakes Pete can bake (integer). For simplicity there are no units for the amounts (e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200). Ingredients that are not present in the objects, can be considered as 0.
+
+// Examples:
+
+// // must return 2
+// cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200});
+// // must return 0
+// cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000});
