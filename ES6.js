57ac634e0300d8a819a053729e0f25357e2af181
Abhishek Duggal <abhiduggal8@gmail.com>
2018-12-09 18:03:50 -0800

    Practice Kata

--- ES6.js
+++ ES6.js
@@ -799,3 +799,56 @@ console.log(
     return Math.floor(val);
   })
 );
+
+// Practice
+
+/*
+  Write a function called getUserById that takes in three parameters: an array of objects (users), an id and a callback, and searches for the user with a matching id.
+  When the correct user object is found, invoke the callback with the user object as an argument.
+*/
+
+// Code here
+var users = [
+  {
+    id: "12d",
+    email: "tyler@gmail.com",
+    name: "Tyler",
+    address: "167 East 500 North"
+  },
+  {
+    id: "15a",
+    email: "cahlan@gmail.com",
+    name: "Cahlan",
+    address: "135 East 320 North"
+  },
+  {
+    id: "16t",
+    email: "ryan@gmail.com",
+    name: "Ryan",
+    address: "192 East 32 North"
+  }
+];
+
+function getUserById(users, id, cb) {
+  //console.log(users);
+  console.log(users[0].id);
+  for (let i = 0; i < users.length; i++) {
+    //console.log(users[i].id);
+    if (users[i].id === id) {
+      return cb(users[i]);
+    }
+  }
+}
+// Do not edit the code below.
+
+getUserById(users, "16t", function(user) {
+  console.log(
+    "The user with the id 16t has the email of " +
+      user.email +
+      " the name of " +
+      user.name +
+      " and the address of " +
+      user.address
+  );
+});
+// Do not edit the code above.
