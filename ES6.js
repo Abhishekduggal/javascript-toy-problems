04ced9480d7c323d4db2ed7e78888c0896a6f7d9
Abhishek Duggal <abhiduggal8@gmail.com>
2018-12-13 19:17:09 -0800

    Practice Kata

--- ES6.js
+++ ES6.js
@@ -958,3 +958,25 @@ getUserById(users, "16t", function(user) {
   );
 });
 // Do not edit the code above.
+
+// Practice
+
+// forEach helper array Function
+
+//Below code to refactor using forEach();
+
+function handlePosts() {
+  var posts = [
+    { id: 23, title: "Daily JS News" },
+    { id: 52, title: "Code Refactor City" },
+    { id: 105, title: "The Brightest Ruby" }
+  ];
+
+  // for (var i = 0; i < posts.length; i++) {
+  //   savePost(posts[i]);
+  // }
+
+  posts.forEach(function(post) {
+    savePost(post);
+  });
+}
