39dbf6fbdf4a470e834d0b6cdf385f06da17219a
Abhishek Duggal <abhiduggal8@gmail.com>
2018-12-21 23:18:20 -0800

    Practice Kata

--- ES6.js
+++ ES6.js
@@ -1178,3 +1178,56 @@ superSize(123456); //654321
 // const mustang = new Car('Ford', 'Mustang', 2001);
 
 // console.log(mustang)
+
+// Practice
+
+// Description:
+
+// Have you heard about the myth that if you fold a paper enough times, you can reach the moon with it? Sure you do, but exactly how many? Maybe it's time to write a program to figure it out.
+
+// You know that a piece of paper has a thickness of 0.0001m. Given distance in units of meters, calculate how many times you have to fold the paper to make the paper reach this distance.
+// (If you're not familiar with the concept of folding a paper: Each fold doubles its total thickness.)
+
+// Note: Of course you can't do half a fold. You should know what this means ;P
+
+// Also, if somebody is giving you a non-positive distance, it's clearly bogus and you should yell at them by returning null/NULL.
+
+//384,400 km to the moon
+
+//https://www.codewars.com/kata/folding-your-way-to-the-moon
+
+// write a function that takes in a number
+//make a counter for paper folds
+// check if number is positive else return null
+// increment counter as we make folds
+// return our counter
+
+function foldsToTheMoon(distance) {
+  let counter = 0;
+  if (distance <= 0) {
+    return null;
+  }
+
+  for (let i = 0.0001; i < distance; i += i) {
+    counter++;
+  }
+  return counter;
+}
+
+foldsToTheMoon(384400000);
+
+// function distanceToMoon(thickness){
+// // let paperThickness = thickness*0.001
+// // console.log(paperThickness)
+// let moon = 384400 * 1000
+// let result = 0;
+
+// if(thickness < 0){
+//   return 'NULL';
+// }else {
+//   result = moon/(thickness)*2
+// }
+// console.log(result);
+// }
+
+// distanceToMoon(0.0001);
