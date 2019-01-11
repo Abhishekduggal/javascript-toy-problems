3a26973f3e173f7f3e52ed8fe4b26418dc73e064
Abhishek Duggal <abhiduggal8@gmail.com>
2019-01-10 19:43:59 -0800

    Practice Kata

--- ES6.js
+++ ES6.js
@@ -1472,3 +1472,31 @@ function translate(str) {
 
 translate("truck");
 //translate('igloo');
+
+// Practice
+
+// Are the numbers in order?
+
+// In this Kata, your function receives an array of positive integers as input. Your task is to determine whether the numbers are in ascending order.
+
+// For the purposes of this Kata, you may assume that all inputs are valid (i.e. arrays containing only positive integers with a length of at least 2).
+
+function inAscOrder(arr) {
+  var first = arr[0];
+  for (var i = 1; i < arr.length; i++) {
+    // if(arr[i] < arr[i - 1])
+    if (first > arr[i]) {
+      return false;
+    } else {
+      first = arr[i];
+      //console.log(i);
+    }
+  }
+  return true;
+}
+// For example:
+
+//inAscOrder([1,2,4,7,19]); // returns true
+//inAscOrder([1,2,3,4,5]); // returns true
+//inAscOrder([1,6,10,18,2,4,20]); // returns false
+//inAscOrder([9,8,7,6,5,4,3,2,1]); // returns false because the numbers are in DESCENDING order
