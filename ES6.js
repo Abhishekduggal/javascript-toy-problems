1ce49318dc1190171afbce25caa939943bac04a4
Abhishek Duggal <abhiduggal8@gmail.com>
2019-01-14 19:54:33 -0800

    Practice Kata

--- ES6.js
+++ ES6.js
@@ -1745,3 +1745,46 @@ _.extend = function(obj) {
   });
   return obj;
 };
+// Like extend, but doesn't ever overwrite a key that already
+  // exists in obj
+  _.defaults = function(obj) {
+    _.each(arguments, function(source){
+        _.each(source, function(value, key){
+          obj[key] === undefined && (obj[key] = value);
+        })
+      });
+      return obj;  
+      
+    }; 
+    
+    /**
+     * FUNCTIONS
+     * =========
+     *
+     * Now we're getting into function decorators, which take in any function
+     * and return out a new version of the function that works somewhat differently
+     */
+  
+    // Return a function that can be called at most one time. Subsequent calls
+    // should return the previously returned value.
+    _.once = function(func) {
+      // TIP: These variables are stored in a "closure scope" (worth researching),
+      // so that they'll remain available to the newly-generated function every
+      // time it's called.
+      var alreadyCalled = false;
+      var result;
+  
+      // TIP: We'll return a new function that delegates to the old one, but only
+      // if it hasn't been called before.
+      return function() {
+        if (!alreadyCalled) {
+          // TIP: .apply(this, arguments) is the standard way to pass on all of the
+          // infromation from one function call to another.
+          result = func.apply(this, arguments);
+          alreadyCalled = true;
+        }
+        // The new function always returns the originally computed result.
+        return result;
+      };
+    };
+  
