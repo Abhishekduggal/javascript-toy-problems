3d32dd90e5b95480a9630108871665753baee375
Abhishek Duggal <abhiduggal8@gmail.com>
2019-01-26 21:55:04 -0800

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -2269,6 +2269,193 @@ console.log(both);
 // // employeeUpdater(employees);
 
 
+// //Code Here
+
+// function removeDuplicates(){
+//   //var arr = [];
+//   var dupArr = workplaceAccidents;
+
+//   for(let i = 0; i < dupArr.length; i++){
+//     //console.log(dupArr[i]);
+//     for(let j = dupArr.length -1; j > i; j--){
+//       //console.log(dupArr[j]);
+//       if(dupArr[i] === dupArr[j]){
+//         dupArr.splice(j,1);
+//       }
+      
+//     }
+//   };
+//   return dupArr;
+// }
+
+// removeDuplicates();
+
+
+////////// PROBLEM 5 //////////
+
+// Do not edit code below.
+// const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax":0.11},
+// {"price":80,"tax":0.11},{"price":69,"tax":0.06},{"price":68,"tax":0.14},
+// {"price":72,"tax":0.14},{"price":51,"tax":0.09},{"price":89,"tax":0.15},
+// {"price":48,"tax":0.13}];
+// // Do not edit code above.
+
+// // /*
+//   Use a higher order method to get the sum of all the order totals after adding in the sales tax
+// // */
+
+// let ordersTotal = orders.map(order => order.price += order.price * order.tax);//console.log(withTax);
+
+// console.log(ordersTotal)
+// //Code Here
+
+
+// function largeNumbers ({first, second, third}) {
+//   return Math.min(first,second,third);
+// };
+
+// largeNumbers({first: 10, second: 2, third: 90})//).toBe(2)
+// var obj = {
+//   lion: 3,
+//   tiger: 4,
+//   bear: 5
+// }
+
+// function zooAnimals(obj){
+//   var {lion, tiger, bear} = {lion: obj.lion, tiger: obj.tiger, bear: obj.bear};
+//   console.l  // /*
+  //   First we'll look at the difference between accessing property values in a for in loop and accessing the property name in a for in loop.
+  //   In the example below, we are accessing the property values. Uncomment the code below, run it and look at what prints in the console.
+  // */
+
+  // var values = {
+  //   one: 'These',
+  //   two: ' are',
+  //   three: ' the',
+  //   four: ' property',
+  //   five: ' values.'
+  // } 
+
+  // for(var key in values) {
+  //   console.log(values[key])
+  // }
+
+  // /*
+  //   In this next example, we are accessing the property names themselves. Uncomment the code below, run it and look at what prints in the console.
+  // */
+
+  // for(var key in values) {
+  //   console.log(key)
+  // }
+
+  // function showValues( obj ) {
+  //   //Code Here
+  //   var str = '';
+
+  //   for(var prop in obj){
+  //     str = str + obj[prop]; 
+  //   }
+  //   return str;
+  // }
+
+  // // function greaterThan10(obj){
+  // //   for (var prop in obj){
+  // //     if(obj[prop] > 10){
+  // //       console.log(obj[prop])
+  // //       obj[prop] = 0;
+  // //     }
+  // //   }
+  // //   return obj;
+  // // }
+  // // var values = {
+  // //   one: 5,
+  // //   two: 12,
+  // //   three: ' the',
+  // //   four: ' property',
+  // //   five: ' values.'
+  // // }
+  // // greaterThan10(values);
+  // // //showValues(values);
+
+// // function totalPopulation(obj){
+
+// //   var{utah, california, texas, arizona} = obj;
+// //   console.log(utah);
+// //   //return (utah.val + california.val + )
+// // }
+
+// // var obj = {utah: 10, california: 29087, texas: 82937, arizona: 28347}
+
+// // totalPopulation(obj);
+
+// // Do not edit the code below.
+// var employees = [
+//   {
+//     "firstName": "Von",
+//     "lastName": "Budibent",
+//     "email": "vbudibent0@163.com",
+//     "department": "Sales"
+//   },
+//   {
+//     "firstName": "Catherina",
+//     "lastName": "Swalowe",
+//     "email": "cswalowe1@example.com",
+//     "department": "Engineering"
+//   },
+//   {
+//     "firstName": "Theo",
+//     "lastName": "Trill",
+//     "email": "ttrill2@sina.com.cn",
+//     "department": "Services"
+//   },
+//   {
+//     "firstName": "Elsy",
+//     "lastName": "McCrorie",
+//     "email": "emccrorie3@netscape.com",
+//     "department": "Legal"
+//   },
+//   {
+//     "firstName": "Lorie",
+//     "lastName": "Handsheart",
+//     "email": "lhandsheart4@fotki.com",
+//     "department": "Research and Development"
+//   }
+// ];
+// // Do not edit the code above.
+
+// /*
+//   Create a function called 'employeeUpdater' that takes no parameters. employeeUpdater will loop over the array above and perform the following:
+//     1. If employee's first name is Theo, remove that employee because he just got fired.
+//     2. If the employee's first name is Lorie, change her department to 'HR'.
+//     3. Return the updated employee array.
+// */
+
+// //Code Here
+
+// // function employeeUpdater() {
+// //   // employees.forEach(function(emp) {
+// //   //   console.log(emp);
+// //   // });
+// // var remove = employees.filter(function(employee){
+// //   console.log(employee.firstName);
+  
+// //   if(employee.firstName === 'Lorie'){
+// //     employee.department = 'HR';
+// //   }
+// //   return employee.firstName !== 'Theo';
+// //   //console.log(employee);
+// // });
+// // return remove;
+// // }
+
+// // employeeUpdater(employees);
+
+og(obj);
+//   return obj;
+  
+// }
+// zooAnimals(obj);
+
 
 
 
