39bf080604b6bad35e86edcd0b1bb143eb27afee
Abhishek Duggal <abhiduggal8@gmail.com>
2019-02-01 21:27:08 -0800

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -2580,7 +2580,41 @@ function myUnshift(array, value){
 myUnshift([1,2,3], 0);
 
 
+// Practice
 
+// https://www.codewars.com/kata/sort-an-array-by-value-and-index/train/javascript
+
+// Your task is to sort an array of integer numbers by the product of the value and the index of the positions. 
+
+// For sorting the index starts at 1, NOT at 0!
+// The sorting has to be ascending.
+// The array will never be null and will always contain numbers. 
+
+// Example:
+
+// Input: 23, 2, 3, 4, 5
+// Product of value and index:
+// 23 => 23 * 1 = 23  -> Output-Pos 4
+// 2 =>  2 * 2 = 4   -> Output-Pos 1
+// 3 =>  3 * 3 = 9   -> Output-Pos 2
+// 4 =>  4 * 4 = 16  -> Output-Pos 3
+// 5 =>  5 * 5 = 25  -> Output-Pos 5
+
+// Output: 2, 3, 4, 23, 5
+
+function sort (arr){
+  let obj = {};
+
+  let newArr = arr.map((el,i) => {
+    obj[el*(i+1)]=el;
+    console.log(obj)
+    });
+  //console.log(newArr)
+  return Object.values(obj);
+
+}
+
+sort([23,2,3,4,5])
 
 
 
