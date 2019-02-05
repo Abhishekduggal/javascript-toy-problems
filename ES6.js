f440c6d5d487bc553535f3ff60e9cc522e100aa2
Abhishek Duggal <abhiduggal8@gmail.com>
2019-02-04 19:24:35 -0800

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -2856,6 +2856,40 @@ og(obj);
 // }
 // zooAnimals(obj);
 
+// ////////// PROBLEM 2 //////////
+
+// // Do not edit the code below.
+// var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
+// // Do not edit the code above.
+
+// /*
+//   The array above represents IDs tied to reported workplace accidents. 
+//   An employee accidentally entered in duplicates to array, making it look as though there are more accidents this year than there actually are.
+//     1. Write a function called 'removeDuplicates' that will remove all duplicate entries in the workplaceAccidents array.
+//     2. Use nested for loops to check for duplicate numbers, and then remove the duplicates from the array.
+//     3. Return the updated array.
+// */
+
+// //Code Here
+
+// function removeDuplicates(){
+//   //var arr = [];
+//   var dupArr = workplaceAccidents;
+
+//   for(let i = 0; i < dupArr.length; i++){
+//     //console.log(dupArr[i]);
+//     for(let j = dupArr.length -1; j > i; j--){
+//       //console.log(dupArr[j]);
+//       if(dupArr[i] === dupArr[j]){
+//         dupArr.splice(j,1);
+//       }
+      
+//     }
+//   };
+//   return dupArr;
+// }
+
+// removeDuplicates();
 
 
 
