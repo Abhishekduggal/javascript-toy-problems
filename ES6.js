6d88615c06d51ac72a14e908359651d9dfd00e3f
Abhishek Duggal <abhiduggal8@gmail.com>
2019-02-19 22:43:25 -0800

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -3360,6 +3360,31 @@ Operator
 !!([false])
 !!([])22
 
+// Practice
+
+function reverseArray(arr){
+  var newArray = [];
+  // console.log(arr);
+  
+  if (arr.length > 0) {
+    // console.log(newArray);
+    newArray.push(arr[arr.length - 1]);
+    // console.log(arr[arr.length - 1]);
+    console.log(newArray);
+    
+    return newArray.concat(reverseArray(arr.slice(0, arr.length-1)))
+  }
+  return [];
+  
+  
+};
+
+var arr = [1,2,3,4]
+var reversedArr = reverseArray(arr);
+
+console.log(reversedArr)    // [4,3,2,1]
+// console.log(arr)    // [1,2,3,4]
+
 
 
     
