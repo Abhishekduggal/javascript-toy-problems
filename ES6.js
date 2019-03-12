03798c5a53a38729bd873f01d8928aa0e4843418
Abhishek Duggal <abhiduggal8@gmail.com>
2019-03-11 21:33:15 -0700

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -3843,4 +3843,21 @@ const nums = { a: 2, b: 3 };
 
 let arr =[]
 console.log(arr.length)
+
+function insertion(arr){
+  for(let i = 1; i < arr.length; i++){
+    // console.log('i',arr[i])
+    for(let j = 0; j < i; j++)
+    // console.log('j', arr[i][j])
+    if(arr[i] < arr[j]){
+      console.log(arr[i], arr[j])
+      let [bigger] = arr.splice(i, 1)
+      arr.splice(j, 0, bigger)
+      j=i
+    }
+  }
+  return arr
+}
+
+insertion([3,2,1])
     
