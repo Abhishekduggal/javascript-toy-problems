23e48e6a5d55e9a2641d60e9ae57ba9e01453224
Abhishek Duggal <abhiduggal8@gmail.com>
2019-03-22 22:16:50 -0700

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -4041,4 +4041,33 @@ foldsToTheMoon(384400000);
 
 // distanceToMoon(0.0001);
 
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+
+// accum("abcd");    ==>  "A-Bb-Ccc-Dddd"
+// accum("RqaEzty"); ==>  "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt");    ==>  "C-Ww-Aaa-Tttt"
+
+const accum = (str) => {
+  var count = 1;
+  var newStr = '';
+  
+  for(var i = 0; i < str.length; i++) {
+      newStr += str.charAt(i).toUpperCase();
+  
+   
+    for(var j = 1; j < count; j++) {
+      newStr += str[i].toLowerCase();
+    }
+    count++;
+  
+    i !== (str.length - 1) ? newStr += `-` : newStr;
+  
+  }
+  return newStr;
+  }
+  
+  accum('cwAt')
+
     
