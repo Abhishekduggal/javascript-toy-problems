2a7c95a27772df901a6565de2a23e0f28db67ede
Abhishek Duggal <abhiduggal8@gmail.com>
2019-04-04 20:53:57 -0700

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -4251,5 +4251,24 @@ sort([23,2,3,4,5])
 
 // sentence
 
+// let dog = {
+//   name: 'Rufus',
+//   isBad: false,
+//   bark: function(){
+//     console.log('woof')
+//   },
+//   age: 77,
+//   color: 'Brindle'
+// };
+
+// let extension = {
+//   isFed: true,
+//   size: 'Big',
+//   isBad : true
+// }
+// let newDog = Object.assign({}, dog, extension);
+// console.log(dog === newDog);
+// console.log(newDog)
+
 
     
