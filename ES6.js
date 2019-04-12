7eeae531db287c2ba93d21435d53975f1deb7ba6
Abhishek Duggal <abhiduggal8@gmail.com>
2019-04-11 20:25:27 -0700

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -4380,6 +4380,34 @@ myCar.tipOver()
 let myToyCar = new Car('Triumph', 'Spitfire', 1976);
 
  
+// groupBy takes an array and splits it into sets, grouped by the 
+// result of running each value through the predicate. If the 
+// predicate is a string instead of a function, it groups by the 
+// property named by predicate on each of the values. 
 
+// function groupBy(collection, predicate){ 
+//   // pseudocode begins here:
+//     // Depending on the second parameter function / string /
+//     // what are we doing here?
+//     // function - call the function and pass each item
+//       // store it in a variable
+//       // They will will be key for the object
+//       // Value will be the item that we are at the iteration of the loop
+      
+//       // check the key alreay exist in the object 
+//         // If it does we have to add, 
+    
+//   // Key  
+//   //& value (array)
+  
+//   // return the new object
+  
+//   //var temp = [];
+  
+//   var key = [];
+//   var result = {};
+//   // Iterate over the array, using forEach
+    
+//       //console.log('Iterate Item', item);
 
     
