fdf97ab7fbfd1e9050d18d92fc04b1b2a95c41d9
Abhishek Duggal <abhiduggal8@gmail.com>
2019-07-22 21:51:58 -0700

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -5708,4 +5708,16 @@ var users = [
     email: 'tyler@gmail.com',
     name: 'Tyler',
     address: '167 East 500 North'
+  },  {
+    id: '15a',
+    email: 'cahlan@gmail.com',
+    name: 'Cahlan',
+    address: '135 East 320 North'
   },
+  {
+    id: '16t',
+    email: 'ryan@gmail.com',
+    name: 'Ryan',
+    address: '192 East 32 North'
+  },
+];
