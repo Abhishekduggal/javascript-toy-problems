6a5eefe2d373687915d4146e13bfc1b134fec6e9
Abhishek Duggal <abhiduggal8@gmail.com>
2019-10-02 21:09:53 -0700

    Practice

--- ES6.js
+++ ES6.js
@@ -6659,3 +6659,21 @@ return counter;
 }
 
 foldsToTheMoon(384400000);
+
+foldsToTheMoon(384400000);
+
+// function distanceToMoon(thickness){
+// // let paperThickness = thickness*0.001
+// // console.log(paperThickness)
+// let moon = 384400 * 1000
+// let result = 0;
+
+// if(thickness < 0){
+//   return 'NULL';
+// }else {
+//   result = moon/(thickness)*2
+// }
+// console.log(result);
+// }
+
+// distanceToMoon(0.0001);
