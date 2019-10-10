658d7ccfe9771501e4b18fe116b5dce15d71f62d
Abhishek Duggal <abhiduggal8@gmail.com>
2019-10-09 22:26:40 -0700

    Practice

--- ES6.js
+++ ES6.js
@@ -6732,3 +6732,12 @@ function timesTable(num1){
 var twelvesTable = timesTable(12);
 twelvesTable(15)    // => 180
 twelvesTable(100)    // => 1200
+
+// Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+// HH = hours, padded to 2 digits, range: 00 - 99
+// MM = minutes, padded to 2 digits, range: 00 - 59
+// SS = seconds, padded to 2 digits, range: 00 - 59
+// The maximum time never exceeds 359999 (99:59:59)
+
+// You can find some examples in the test fixtures.
