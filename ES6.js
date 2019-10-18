43f87faa3a0c8532ec1aca6c84de69079b3f95dd
Abhishek Duggal <abhiduggal8@gmail.com>
2019-10-17 21:50:16 -0700

    Practice

--- ES6.js
+++ ES6.js
@@ -6809,3 +6809,14 @@ tellMeTime(3599);
 
 // The input consists of a single non-empty string, consisting only of uppercase English letters, the string's length doesn't exceed 200 characters
 
+// Examples:
+// songDecoder("WUBWUBIWUBAMWUBWUBX") ==> I AM X
+// songDecoder("WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB") ==>  WE ARE THE CHAMPIONS MY FRIEND
+
+function songDecoder(str){
+  let arr = str.split('WUB');
+  //console.log(arr);
+
+//   let songs = arr.filter(song => {
+//     return (song !== 'W' && song !=='U' && song !=='B')
+//   })
