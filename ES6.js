704028e7b993c51d8c8e7fb76dc1cc4c88cc0d79
Abhishek Duggal <abhiduggal8@gmail.com>
2019-10-19 13:46:26 -0700

    Practice

--- ES6.js
+++ ES6.js
@@ -6831,3 +6831,9 @@ return arr.filter(s=>s).join(' ');
 
 //songDecoder("WUBWUBIWUBAMWUBWUBX");
 songDecoder("WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB")
+
+let arr1 = [1,2,3,4,5];
+let arr2 = [6,7,8,9];
+
+let both = [...arr1, ...arr2];
+console.log(both);
