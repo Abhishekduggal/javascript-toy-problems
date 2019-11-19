9c3337afa0329089eafef5ebc0786d58f1acc28a
Abhishek Duggal <abhiduggal8@gmail.com>
2019-11-18 17:48:34 -0800

    Practice

--- ES6.js
+++ ES6.js
@@ -7203,3 +7203,17 @@ rocky.speak();
 //   year: "2018",
 //   miles: 0
 // }
+
+// Count down 3 times to an positive integer n, return these 3 numbers as a string, separated by exclamation marks.
+// Code Limit: 30 Characters
+
+// Example
+// n = 1  // ==>  "3!2!1"
+//n = 10  //==>  "12!11!10"
+// n = 100 //==>  "102!101!100"
+
+function countdown(n){
+  //30 Character solution goes here
+  let arrN = [n];
+  // let addN = [n+1, n+2];
+  
