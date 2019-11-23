3abdb5f15bc620ded4ee1d39f72eb4f96db599ea
Abhishek Duggal <abhiduggal8@gmail.com>
2019-11-23 00:50:16 -0800

    Practice

--- ES6.js
+++ ES6.js
@@ -7259,3 +7259,18 @@ function unformatted() {
   
   
   unformatted();
+
+
+  // This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+
+// accum("abcd");    ==>  "A-Bb-Ccc-Dddd"
+// accum("RqaEzty"); ==>  "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt");    ==>  "C-Ww-Aaa-Tttt"
+
+const accum = (str) => {
+  var count = 1;
+  var newStr = '';
+  
+  
