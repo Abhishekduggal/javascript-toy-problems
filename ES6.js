5cd366586cd6fe9665cf5b8ec6e8353161f2d97b
Abhishek Duggal <abhiduggal8@gmail.com>
2019-12-05 22:40:03 -0800

    Practice

--- ES6.js
+++ ES6.js
@@ -7390,3 +7390,12 @@ for(var i = 0; i< str.length; i++){
 
 let arr =[]
 console.log(arr.length)
+
+// The rgb() method is incomplete. Complete the method so that passing in RGB decimal values will result in a hexadecimal representation being returned. The valid decimal values for RGB are 0 - 255. Any (r,g,b) argument values that fall out of that range should be rounded to the closest valid value.
+
+// The following are examples of expected output values:
+
+// rgb(255, 255, 255) // returns FFFFFF
+// rgb(255, 255, 300) // returns FFFFFF
+// rgb(0,0,0) // returns 000000
+// rgb(148, 0, 211) // returns 9400D3
