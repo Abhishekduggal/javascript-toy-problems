d29b21315114de84b0f542ef5e29da06f51801de
Abhishek Duggal <abhiduggal8@gmail.com>
2019-12-06 22:58:30 -0800

    Practice

--- ES6.js
+++ ES6.js
@@ -7399,3 +7399,18 @@ console.log(arr.length)
 // rgb(255, 255, 300) // returns FFFFFF
 // rgb(0,0,0) // returns 000000
 // rgb(148, 0, 211) // returns 9400D3
+
+// ////////// PROBLEM 2 //////////
+
+// // Do not edit the code below.
+// var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
+// // Do not edit the code above.
+
+// /*
+//   The array above represents IDs tied to reported workplace accidents. 
+//   An employee accidentally entered in duplicates to array, making it look as though there are more accidents this year than there actually are.
+//     1. Write a function called 'removeDuplicates' that will remove all duplicate entries in the workplaceAccidents array.
+//     2. Use nested for loops to check for duplicate numbers, and then remove the duplicates from the array.
+//     3. Return the updated array.
+// */
+
