fa137f47f2a3c353ba79a7843c316e391b01819c
Abhishek Duggal <abhiduggal8@gmail.com>
2019-12-17 21:54:58 -0800

    Practice

--- ES6.js
+++ ES6.js
@@ -7563,3 +7563,21 @@ console.log(arr.length)
   //   }
   //   return str;
   // }
+// // function greaterThan10(obj){
+  // //   for (var prop in obj){
+  // //     if(obj[prop] > 10){
+  // //       console.log(obj[prop])
+  // //       obj[prop] = 0;
+  // //     }
+  // //   }
+  // //   return obj;
+  // // }
+  // // var values = {
+  // //   one: 5,
+  // //   two: 12,
+  // //   three: ' the',
+  // //   four: ' property',
+  // //   five: ' values.'
+  // // }
+  // // greaterThan10(values);
+  // // //showValues(values);
