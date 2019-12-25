dc0cf52d5c201f14e11c54e678ab861e2c327311
Abhishek Duggal <abhiduggal8@gmail.com>
2019-12-24 18:42:45 -0800

    Practice

--- ES6.js
+++ ES6.js
@@ -7695,3 +7695,27 @@ console.log(arr.length)
 // // Do not edit the code above.
 
 // /*
+
+// /*
+//   Create a function called 'employeeUpdater' that takes no parameters. employeeUpdater will loop over the array above and perform the following:
+//     1. If employee's first name is Theo, remove that employee because he just got fired.
+//     2. If the employee's first name is Lorie, change her department to 'HR'.
+//     3. Return the updated employee array.
+// */
+
+// //Code Here
+
+// // function employeeUpdater() {
+// //   // employees.forEach(function(emp) {
+// //   //   console.log(emp);
+// //   // });
+// // var remove = employees.filter(function(employee){
+// //   console.log(employee.firstName);
+  
+// //   if(employee.firstName === 'Lorie'){
+// //     employee.department = 'HR';
+// //   }
+// //   return employee.firstName !== 'Theo';
+// //   //console.log(employee);
+// // });
+// // return remove;
