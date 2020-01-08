31f0076afe3d0a5b6f3e699fed082d6688d3b54b
Abhishek Duggal <abhiduggal8@gmail.com>
2020-01-07 22:36:00 -0800

    Practice

--- ES6.js
+++ ES6.js
@@ -7984,3 +7984,12 @@ function fizzBuzz(n){
 }
 
 fizzBuzz(60);
+
+// Write a function called songDecoder that removes the terrible dubstep sounds from a song, to reveal the original, pure, un-dubstepped beauty.
+
+// The input consists of a single non-empty string, consisting only of uppercase English letters, the string's length doesn't exceed 200 characters
+
+// Examples:
+// songDecoder("WUBWUBIWUBAMWUBWUBX") ==> I AM X
+// songDecoder("WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB") ==>  WE ARE THE CHAMPIONS MY FRIEND
+
