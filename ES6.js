edf58f5214ee2535d697415dcaa17d914d1549eb
Abhishek Duggal <abhiduggal8@gmail.com>
2020-01-31 23:57:20 -0800

    Practice

--- ES6.js
+++ ES6.js
@@ -8272,3 +8272,15 @@ function insertion(arr){
 // }
 
 // removeDuplicates();
+
+
+////////// PROBLEM 5 //////////
+
+// Do not edit code below.
+// const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax":0.11},
+// {"price":80,"tax":0.11},{"price":69,"tax":0.06},{"price":68,"tax":0.14},
+// {"price":72,"tax":0.14},{"price":51,"tax":0.09},{"price":89,"tax":0.15},
+// {"price":48,"tax":0.13}];
+// // Do not edit code above.
+
+// // /*
