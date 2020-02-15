4c6d121f0f97a9e074620bfe8c949e203aa1ba02
Abhishek Duggal <abhiduggal8@gmail.com>
2020-02-14 22:05:20 -0800

    Practice

--- ES6.js
+++ ES6.js
@@ -8482,4 +8482,25 @@ let z = Array.from(n.toString()).map(Number);
   //is already the maximum possible integer, your function 
   //should simply return it.
 
-  
+
+
+
+// groupBy takes an array and splits it into sets, grouped by the 
+// result of running each value through the predicate. If the 
+// predicate is a string instead of a function, it groups by the 
+// property named by predicate on each of the values. 
+
+// function groupBy(collection, predicate){ 
+//   // pseudocode begins here:
+//     // Depending on the second parameter function / string /
+//     // what are we doing here?
+//     // function - call the function and pass each item
+//       // store it in a variable
+//       // They will will be key for the object
+//       // Value will be the item that we are at the iteration of the loop
+      
+//       // check the key alreay exist in the object 
+//         // If it does we have to add, 
+    
+//   // Key  
+//   //& value (array)
