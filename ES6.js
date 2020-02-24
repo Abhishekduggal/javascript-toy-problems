95fe8078473ebd5e3141626e782dc3dcbfa2b166
Abhishek Duggal <abhiduggal8@gmail.com>
2020-02-23 21:11:30 -0800

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -8688,3 +8688,16 @@ function superSize(int) {
   let y = z.sort(function (a, b) { return b - a });
   console.log(y);
 }
+
+
+(n.toString()).map(Number);
+//console.log(z);
+let y = z.sort(function (a, b) { return b - a });
+console.log(y);
+}
+superSize(123456) //654321
+//superSize(105) // 510
+//superSize(12) // 21
+// If the argument passed through is single digit or is 
+//already the maximum possible integer, your function 
+//should simply return it.
