4c5bf8d9f8988c04985939db55d7d797868553db
Abhishek Duggal <abhiduggal8@gmail.com>
2020-02-29 12:49:17 -0800

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -8781,3 +8781,14 @@ truthyOrFalsy(null);
 // logs 'The value is falsy';
 truthyOrFalsy(NaN);
 // logs 'The value is falsy';
+
+// Write a function called songDecoder that removes the terrible 
+//dubstep sounds from a song, to reveal the original, pure, un-dubstepped beauty.
+
+// The input consists of a single non-empty string, consisting only of 
+//uppercase English letters, the string's length doesn't exceed 200 characters
+
+// Examples:
+// songDecoder("WUBWUBIWUBAMWUBWUBX") ==> I AM X
+// songDecoder
+//("WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB") ==>  WE ARE THE CHAMPIONS MY FRIEND
