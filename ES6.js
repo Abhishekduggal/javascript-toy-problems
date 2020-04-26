8eed04882b32f0d0d7106aea8bbcc1aad2525972
Abhishek Duggal <abhiduggal8@gmail.com>
2020-04-25 22:57:07 -0700

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -9143,3 +9143,17 @@ function twoOldestAges(ages) {
 // });
 
 twoOldestAges([1, 2, 10, 8, 32, 44, 45, 66]) // should return [8, 10];
+
+
+// Are the numbers in order?
+
+// In this Kata, your function receives an array of positive integers as input. Your task is to determine whether the numbers are in ascending order.
+
+// For the purposes of this Kata, you may assume that all inputs are valid (i.e. arrays containing only positive integers with a length of at least 2).
+
+function inAscOrder(arr) {
+  var first = arr[0];
+  for (var i = 1; i < arr.length; i++) {
+    // if(arr[i] < arr[i - 1])
+    if (first > arr[i]) {
+      return false;
