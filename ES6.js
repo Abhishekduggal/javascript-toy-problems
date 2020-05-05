04b8cdab2e7bc9ed10237144a5093fddb74dffb0
Abhishek Duggal <abhiduggal8@gmail.com>
2020-05-04 22:25:55 -0700

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -9284,3 +9284,26 @@ function inAscOrder(arr) {
           //console.log(key);
           key = firstLetter(item);
           output[key] = item;
+
+          //console.log('output', output);
+
+        } else if (typeof item === 'number') {
+          //console.log('hello world');
+          key = mathValue(item);
+          //console.log(key);
+          output[key] = item;
+          //console.log('output', output);
+        }
+      } else if (typeof predicate === 'string') {
+        //console.log(item.length);
+        //console.log(key);
+        key = item.length;
+        output[key] = item;
+        //console.log('output', output);
+      }
+    });
+
+    return output;
+    // //console.log('output', output);
+
+  }
