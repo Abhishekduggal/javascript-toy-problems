423f66df40b6f9a89cb78b95011cf62aebbd665d
Abhishek Duggal <abhiduggal8@gmail.com>
2020-05-23 23:26:14 -0700

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -9560,3 +9560,9 @@ function HashTable(size) {
   this.buckets = Array(size);
   this.numBuckets = this.buckets.length;
 }
+
+function HashNode(key, value, next) {
+  this.key = key;
+  this.value = value;
+  this.next = next || null;
+}
