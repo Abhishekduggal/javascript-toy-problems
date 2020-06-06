ec3f53a15acc20502175023c2bc194fd566f23a8
Abhishek Duggal <abhiduggal8@gmail.com>
2020-06-05 23:23:46 -0700

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -9708,3 +9708,13 @@ HashTable.prototype.get = function (key) {
       }
       return true;
     }
+    return true;
+  }
+  // For example:
+  
+  //inAscOrder([1,2,4,7,19]); // returns true
+  //inAscOrder([1,2,3,4,5]); // returns true
+  //inAscOrder([1,6,10,18,2,4,20]); // returns false
+  //inAscOrder([9,8,7,6,5,4,3,2,1]); // returns false because the numbers are in DESCENDING order
+  
+  // Extra Challenge: If time, try to optimise and shorten your code as much as possible.
