32e83389b413c0a8d1627a637ff4f2da621d0652
Abhishek Duggal <abhiduggal8@gmail.com>
2020-08-02 23:44:53 -0700

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -10398,3 +10398,8 @@ HashTable.prototype.get = function (key) {
 
 
 
+
+
+// Write a function that, given a string, returns the number of vowels in that string.
+
+// We will consider that a, e, i, o and u are vowels for the sake of this problem.
