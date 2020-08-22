4a22e9b411d370521827f673de53e5a80d09bde8
Abhishek Duggal <abhiduggal8@gmail.com>
2020-08-22 00:14:13 -0700

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -10684,3 +10684,24 @@ HashTable.prototype.get = function (key) {
 //         dupArr.splice(j,1);
 //       }
 
+
+//     }
+//   };
+//   return dupArr;
+// }
+
+// removeDuplicates();
+
+
+////////// PROBLEM 5 //////////
+
+// Do not edit code below.
+// const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax":0.11},
+// {"price":80,"tax":0.11},{"price":69,"tax":0.06},{"price":68,"tax":0.14},
+// {"price":72,"tax":0.14},{"price":51,"tax":0.09},{"price":89,"tax":0.15},
+// {"price":48,"tax":0.13}];
+// // Do not edit code above.
+
+// // /*
+//   Use a higher order method to get the sum of all the order totals after adding in the sales tax
+// // */
