adea45103c40c2d300852e243c0f2facbc9bd6ed
Abhishek Duggal <abhiduggal8@gmail.com>
2020-09-10 23:02:26 -0700

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -10867,3 +10867,18 @@ HashTable.prototype.get = function (key) {
 
 // /*
 //   Create a function called 'employeeUpdater' that takes no parameters. employeeUpdater will loop over the array above and perform the following:
+
+
+//     1. If employee's first name is Theo, remove that employee because he just got fired.
+//     2. If the employee's first name is Lorie, change her department to 'HR'.
+//     3. Return the updated employee array.
+// */
+
+// //Code Here
+
+// // function employeeUpdater() {
+// //   // employees.forEach(function(emp) {
+// //   //   console.log(emp);
+// //   // });
+// // var remove = employees.filter(function(employee){
+// //   console.log(employee.firstName);
