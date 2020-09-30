ed10bba20d2244f48a7600bbf16da5cfa5d8f45f
Abhishek Duggal <abhiduggal8@gmail.com>
2020-09-29 19:19:03 -0700

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -11035,4 +11035,12 @@ foldsToTheMoon(384400000);
 
 // distanceToMoon(0.0001);
 
+// var names = ['Alexandria', 'Matthew', 'Joe']
 
+// names.every(function (name){
+//   return name.length >4 
+// })
+
+// names.some(function (name){
+//   return name.length >4 
+// })
