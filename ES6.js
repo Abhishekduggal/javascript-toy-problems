40c0eec7c6f231f93eb6a40b76314bc6cbe96132
Abhishek Duggal <abhiduggal8@gmail.com>
2020-10-16 22:27:19 -0700

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -11234,3 +11234,15 @@ function assignEvenAndOdds(userInput) {
   console.log(arr);
   var evens = [];
   var odds = [];
+
+  userInput.filter(num => {
+    if (parseInt(num) % 2 === 0) {
+      evens.push(num);
+    } else if (parseInt(num) % 2 !== 0) {
+      odds.push(num);
+    }
+  });
+  console.log(evens);
+  console.log(odds);
+}
+assignEvenAndOdds(['1', '2', '3', '4'])
