8c9ac25ca6af7bf7f190690ade92070fd8972a57
Abhishek Duggal <abhiduggal8@gmail.com>
2020-10-25 12:10:52 -0700

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -11334,3 +11334,12 @@ function findFirstLargeNumber(element) {
 }
 
 console.log(array2.findIndex(findFirstLargeNumber))
+
+var array1 = [5, 12, 8, 130, 44];
+
+var found = array1.find(function (element) {
+  return element > 10;
+});
+
+console.log(found);
+// expected output: 12
