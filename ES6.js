9890a09f8175d056d5a07e4529499cc108a023dc
Abhishek Duggal <abhiduggal8@gmail.com>
2020-10-28 11:59:37 -0700

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -11367,3 +11367,14 @@ var noDogsArra = specialPets.filter(function (val) {
 console.log(noDogsArra);
 // [ { name: 'Sadie', species: 'cat' },
 //   { name: 'Layla', species: 'cat' } ]
+
+
+// Write a function that takes in an array of integers and removes the lowest value, then returns the adjusted array.  If there are multiple instances of the same number, remove the number with the lowest index.  If passed an empty array, your function should return an empty array.
+
+// Do not change the order of the remaining elements of the array.
+
+// EXAMPLE:
+
+// var sampleArr = [9, 2, 7, 3, 1, 5, 6, 4, 8, 1]
+
+// Function should return: [9, 2, 7, 3, 5, 6, 4, 8, 1]
