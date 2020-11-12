7c8c7f2312afc078a0b6537e583660c48ecbe050
Abhishek Duggal <abhiduggal8@gmail.com>
2020-11-12 12:57:21 -0800

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -11580,3 +11580,19 @@ LinkedList.prototype.addToTail = function (value) {
   }
   this.tail = newNode;
 };
+
+LinkedList.prototype.removeHead = function () {
+  if (!this.head) {
+    return null;
+  }
+
+  var val = this.head.value;
+  this.head = this.head.next;
+
+  if (this.head) {
+    this.head.prev = null;
+  } else {
+    this.tail = null;
+  }
+  return val;
+};
