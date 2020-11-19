1b04e7b39bccc17b5c1e39b5c74e3e6c71e13d1a
Abhishek Duggal <abhiduggal8@gmail.com>
2020-11-19 13:38:39 -0800

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -11689,3 +11689,13 @@ console.log(myLL.indexOf(10));
 // The array will never be null and will always contain numbers. 
 
 // Example:
+
+// Input: 23, 2, 3, 4, 5
+// Product of value and index:
+// 23 => 23 * 1 = 23  -> Output-Pos 4
+// 2 =>  2 * 2 = 4   -> Output-Pos 1
+// 3 =>  3 * 3 = 9   -> Output-Pos 2
+// 4 =>  4 * 4 = 16  -> Output-Pos 3
+// 5 =>  5 * 5 = 25  -> Output-Pos 5
+
+// Output: 2, 3, 4, 23, 5
