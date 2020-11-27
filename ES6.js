24e980db282e97910fa0b7d224e894af77168b18
Abhishek Duggal <abhiduggal8@gmail.com>
2020-11-27 14:54:20 -0800

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -11800,3 +11800,14 @@ sumDigits(1234)    // => 10
 //   var key = [];
 //   var result = {};
 //   // Iterate over the array, using forEach
+
+//       //console.log('Iterate Item', item);
+
+//     if(typeof predicate === 'function'){
+//         collection.forEach(function(item){
+//           if(result[predicate(item)]){
+//             result[predicate(item)].push(item);
+//           }else{
+//             result[predicate(item)] = item;
+//           }
+//         });  
