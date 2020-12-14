1533e20595dcbb5da4d6f55670b74ab367dc77cd
Abhishek Duggal <abhiduggal8@gmail.com>
2020-12-14 14:03:56 -0800

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -11990,3 +11990,16 @@ function timesTable(num1) {
 var twelvesTable = timesTable(12);
 twelvesTable(15)    // => 180
 twelvesTable(100)    // => 1200
+
+
+// Your task is to write function toLeetSpeak that converts a regular english sentence to Leetspeak.
+
+// More about LeetSpeak You can read at wiki -> https://en.wikipedia.org/wiki/Leet
+
+// Consider only uppercase letters (no lowercase letters, no numbers) and spaces.
+
+// For example:
+
+// toLeetSpeak("LEET") returns "1337"
+// In this kata we use a simple LeetSpeak dialect. Use this alphabet:
+
