5cd5224951e1b3a663ea38bd3352aabf4006173a
Abhishek Duggal <abhiduggal8@gmail.com>
2020-12-17 15:47:13 -0800

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -12031,3 +12031,7 @@ const leet = {
 const toLeetSpeak = str => str.split('').map(e => leet[e]).join('');
 
 toLeetSpeak("LEET")
+
+// The rgb() method is incomplete. Complete the method so that passing in RGB decimal values will result in a hexadecimal representation being returned. The valid decimal values for RGB are 0 - 255. Any (r,g,b) argument values that fall out of that range should be rounded to the closest valid value.
+
+// The following are examples of expected output values:
