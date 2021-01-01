f8093ea9e588f9d29f3cb7ef1845735fa06b2ac1
Abhishek Duggal <abhiduggal8@gmail.com>
2020-12-31 18:45:53 -0800

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -12199,3 +12199,15 @@ function exponentiate(value1, value2) {
 }
 
 exponentiate(3, 4)
+
+
+/* 
+
+An isogram is a word that has no repeating letters, consecutive or nonconsecutive. Create a function that takes a string and returns either true or false depending on whether or not it's an "isogram". Assume all inputs will be lowercase.
+
+For example:
+console.log(isIsogram('algorithm')); // returns true
+console.log(isIsogram('do the max')); // returns false (multiple spaces)
+console.log(isIsogram('hello there')); // returns false (multiple l's)
+
+*/
