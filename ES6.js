422471dd4e12eaaf23f50028eb1ef6e2298258b6
Abhishek Duggal <abhiduggal8@gmail.com>
2021-01-03 21:26:47 -0800

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -12211,3 +12211,13 @@ console.log(isIsogram('do the max')); // returns false (multiple spaces)
 console.log(isIsogram('hello there')); // returns false (multiple l's)
 
 */
+
+function isIsogram(str) {
+  // split the alphabets
+  // for loop
+  // if / else
+
+  var split = str.split('');
+
+  var track = [];
+//console.log(split);
