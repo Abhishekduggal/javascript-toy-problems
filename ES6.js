941a50a345993992dd3e2c47b167932bf4091000
Abhishek Duggal <abhiduggal8@gmail.com>
2021-01-21 12:02:41 -0800

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -12410,3 +12410,19 @@ function underToCamel(underName) {
     }
     return val;
   };
+
+  LinkedList.prototype.removeTail = function () {
+    if (!this.tail) {
+      return null;
+    }
+
+    var val = this.tail.value;
+    this.tail = this.tail.prev;
+
+    if (this.tail) {
+      this.tail.next = null;
+    } else {
+      this.head = null;
+    }
+    return val;
+  };
