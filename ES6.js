8e0f91e9164f59d56685d47a3617d80bea1c3098
Abhishek Duggal <abhiduggal8@gmail.com>
2021-03-15 16:44:55 -0700

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -13007,3 +13007,9 @@ function underToCamel(underName) {
     console.log(odds);
   }
   assignEvenAndOdds(['1', '2', '3', '4'])
+
+  // Pete likes to bake some cakes. He has some recipes and ingredients. 
+  //Unfortunately he is not good in maths. Can you help him to find out, 
+  //how many cakes he could bake considering his recipes?
+
+
