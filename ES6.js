d857f7226ac0ac5314af904e924be84aa95ce061
Abhishek Duggal <abhiduggal8@gmail.com>
2021-03-20 14:59:34 -0700

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -13025,8 +13025,13 @@ function underToCamel(underName) {
 
   // Examples:
 
-// // must return 2
-// cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}); 
-// // must return 0
-// cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000});
+  // // must return 2
+  // cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}); 
+  // // must return 0
+  // cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000});
 
+
+
+  function countGrade(scores) {
+    //coding here...
+    var S = scores.filter(function (x) { return x === 100 }).length;
