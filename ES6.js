b66976655ee6b6f981ff09495fefdc44ab22dbc9
Abhishek Duggal <abhiduggal8@gmail.com>
2021-04-12 14:02:20 -0700

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -13224,4 +13224,17 @@ function underToCamel(underName) {
     //console.log(split);
 
     for (var i = 0; i < split.length; i++) {
-    //str.indexOf(searchValue[, fromIndex])]
+      //str.indexOf(searchValue[, fromIndex])]
+
+      //console.log(track);
+      //console.log(track.indexOf(split[i]));
+      if (track.indexOf(split[i]) > -1) {
+        return false;
+      }
+      track.push(split[i]);
+    }
+    return true;
+  }
+
+  console.log(isIsogram('algorithm'));
+  console.log(isIsogram('do the max'));
