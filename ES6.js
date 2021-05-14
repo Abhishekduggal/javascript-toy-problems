733af170775b5a430bca484ccb0be8d6b468785c
Abhishek Duggal <abhiduggal8@gmail.com>
2021-05-14 06:58:59 -0700

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -13423,62 +13423,73 @@ function underToCamel(underName) {
   // // product
 
   / let customers = [
-  //   {name: "Shea", total: 150, message: 'This '},
-  //   {name: 'Jarid', total: 1, message: 'is '},
-  //   {name: 'Joe', total: 10000, message: 'a new '},
-  //   {name: 'Erik', total: -100, message: 'message.'}
-  // ]
+    //   {name: "Shea", total: 150, message: 'This '},
+    //   {name: 'Jarid', total: 1, message: 'is '},
+    //   {name: 'Joe', total: 10000, message: 'a new '},
+    //   {name: 'Erik', total: -100, message: 'message.'}
+    // ]
 
-  // // let total = customers.reduce( function(total, elem){
-  // //   return total + elem.total; 
-  // // }, 0)
-
-
-
-  // let customers = [
-//   {name: "Shea", total: 150, message: 'This '},
-//   {name: 'Jarid', total: 1, message: 'is '},
-//   {name: 'Joe', total: 10000, message: 'a new '},
-//   {name: 'Erik', total: -100, message: 'message.'}
-// ]
-
-// // let total = customers.reduce( function(total, elem){
-// //   return total + elem.total; 
-// // }, 0)
-
-// let sentence = customers.reduce(function(total, elem){
-//   return total + elem.message;
-// }, '')
-
-// sentence
-
-
-// let dog = {
-//   name: 'Rufus',
-//   isBad: false,
-//   bark: function(){
-//     console.log('woof')
-//   },
-
-// Pete likes to bake some cakes. He has some recipes and ingredients. Unfortunately he is not good in maths. Can you help him to find out, how many cakes he could bake considering his recipes?
-
-// Write a function cakes(), which takes the recipe (object) and the available ingredients (also an object) and returns the maximum number of cakes Pete can bake (integer). For simplicity there are no units for the amounts (e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200). Ingredients that are not present in the objects, can be considered as 0.
-
-// Examples:
-
-
-// Examples:
-
-// // must return 2
-// cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}); 
-// // must return 0
-// cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000});
+    // // let total = customers.reduce( function(total, elem){
+    // //   return total + elem.total; 
+    // // }, 0)
 
 
 
-// groupBy takes an array and splits it into sets, grouped by the 
-// result of running each value through the predicate. If the 
-// predicate is a string instead of a function, it groups by the 
-// property named by predicate on each of the values. 
+    // let customers = [
+    //   {name: "Shea", total: 150, message: 'This '},
+    //   {name: 'Jarid', total: 1, message: 'is '},
+    //   {name: 'Joe', total: 10000, message: 'a new '},
+    //   {name: 'Erik', total: -100, message: 'message.'}
+    // ]
 
-// function groupBy(collection, predicate){ 
+    // // let total = customers.reduce( function(total, elem){
+    // //   return total + elem.total; 
+    // // }, 0)
+
+    // let sentence = customers.reduce(function(total, elem){
+    //   return total + elem.message;
+    // }, '')
+
+    // sentence
+
+
+    // let dog = {
+    //   name: 'Rufus',
+    //   isBad: false,
+    //   bark: function(){
+    //     console.log('woof')
+    //   },
+
+    // Pete likes to bake some cakes. He has some recipes and ingredients. Unfortunately he is not good in maths. Can you help him to find out, how many cakes he could bake considering his recipes?
+
+    // Write a function cakes(), which takes the recipe (object) and the available ingredients (also an object) and returns the maximum number of cakes Pete can bake (integer). For simplicity there are no units for the amounts (e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200). Ingredients that are not present in the objects, can be considered as 0.
+
+    // Examples:
+
+
+    // Examples:
+
+    // // must return 2
+    // cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}); 
+    // // must return 0
+    // cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000});
+
+
+
+    // groupBy takes an array and splits it into sets, grouped by the 
+    // result of running each value through the predicate. If the 
+    // predicate is a string instead of a function, it groups by the 
+    // property named by predicate on each of the values. 
+
+    // function groupBy(collection, predicate){ 
+
+    /   // pseudocode begins here:
+  //     // Depending on the second parameter function / string /
+  //     // what are we doing here?
+  //     // function - call the function and pass each item
+  //       // store it in a variable
+  //       // They will will be key for the object
+  //       // Value will be the item that we are at the iteration of the loop
+
+  //       // check the key alreay exist in the object 
+  //         // If it does we have to add, 
