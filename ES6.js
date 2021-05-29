b8320040462d3213a5b8a7e4e45d1612a23f47c6
Abhishek Duggal <abhiduggal8@gmail.com>
2021-05-29 12:11:31 -0700

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -13598,3 +13598,13 @@ function underToCamel(underName) {
   }));
 // what would this return?
 // returns { '5': [5.6], '3':[3.6, 3.4], '1':[1.4,1.8]};
+
+
+// Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+// HH = hours, padded to 2 digits, range: 00 - 99
+// MM = minutes, padded to 2 digits, range: 00 - 59
+// SS = seconds, padded to 2 digits, range: 00 - 59
+// The maximum time never exceeds 359999 (99:59:59)
+
+// You can find some examples in the test fixtures.
