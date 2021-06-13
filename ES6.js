5c6708735dc92855d8fadf4c50968a3c548550ae
Abhishek Duggal <abhiduggal8@gmail.com>
2021-06-13 10:18:45 -0700

    Practice Problem

--- ES6.js
+++ ES6.js
@@ -13688,3 +13688,17 @@ function underToCamel(underName) {
   }
 
   countGrade([50, 60, 70, 80, 90, 100]); //should return {S:1, A:1, B:1, C:2, D:1, X:0}
+
+  // ////////// PROBLEM 2 //////////
+
+// // Do not edit the code below.
+// var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
+// // Do not edit the code above.
+
+// /*
+//   The array above represents IDs tied to reported workplace accidents. 
+//   An employee accidentally entered in duplicates to array, making it look as though there are more accidents this year than there actually are.
+//     1. Write a function called 'removeDuplicates' that will remove all duplicate entries in the workplaceAccidents array.
+//     2. Use nested for loops to check for duplicate numbers, and then remove the duplicates from the array.
+//     3. Return the updated array.
+// */
